63d5f1462b0eed6f41f35245b225ea7c856687f9
maksim <maksimilianandreev@gmail.com>
2024-03-26 20:09:00 +0300

    вход регистрация траляля

--- app/app.py
+++ app/app.py
@@ -1,36 +1,74 @@
-from flask import Flask, render_template, request
-from flask_login import LoginManager, login_user, logout_user, UserMixin
+from flask import Flask, render_template, request, redirect
+from flask_login import LoginManager, login_user, logout_user, login_required
+from data.users import User
+from data import db_session
+from forms.login import LoginForm
+from forms.user import RegisterForm
 
 
 app = Flask(__name__)
-login_manager = LoginManager(app)
+app.config['SECRET_KEY'] = 'secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return ''
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.language.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    # Реализация страницы входа пользователя
-    if request.method == 'POST':
-        # Проверка логина и пароля пользователя
-        user = User.query.filter_by(username=request.form['username']).first()
-        if user and check_password_hash(user.password, request.form['password']):
-            login_user(user)
-            return redirect(url_for('profile'))
-        else:
-            flash('Invalid username or password')
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
-    return render_template('login.html')
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == "__main__":
+    db_session.global_init("db/bs.db")
     app.run(host='0.0.0.0', port=1234)
--- app/data/users.py
+++ app/data/users.py
@@ -1,7 +1,6 @@
 import datetime
 import sqlalchemy
-from db_session import SqlAlchemyBase
-from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
@@ -12,13 +11,12 @@ class User(SqlAlchemyBase, UserMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    languages = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
                               index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime,
                                      default=datetime.datetime.now)
-    news = orm.relationship("News", back_populates='user')
 
     def __str__(self):
         return f'{self.id} {self.name} {self.email}'
